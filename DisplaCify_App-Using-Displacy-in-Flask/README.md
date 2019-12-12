304304cbc196211a0b6c7897dda18ddd42398e61
JCharis <jcharistech@gmail.com>
2019-12-12 10:12:21 +0200

    DisplaCify - Using Displacy in Flask

---
+++ DisplaCify_App-Using-Displacy-in-Flask/README.md
@@ -0,0 +1,25 @@
+### DisplaCify App
+#### Using Displacy in Flask
+
++ This is to demonstrate how to render Named Entity Recognition with Displacy in Flask
+
+#### Requirements
++ Flask
++ Flask-Markdown
++ Spacy
+
+
+#### Images
+![](images/screenshot01.png)
+
+
+![](images/screenshot01.png)
+
+
+
+![](images/screenshot01.png)
+
+
+#### By
++ Jesse E.Agbe(JCharis)
++ Jesus Saves@JCharisTech
---
+++ DisplaCify_App-Using-Displacy-in-Flask/app.py
@@ -0,0 +1,55 @@
+from flask import Flask,url_for,render_template,request
+import spacy
+from spacy import displacy
+nlp = spacy.load('en')
+import json
+
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
+
+from flaskext.markdown import Markdown
+
+app = Flask(__name__)
+Markdown(app)
+
+
+# def analyze_text(text):
+# 	return nlp(text)
+
+@app.route('/')
+def index():
+	# raw_text = "Bill Gates is An American Computer Scientist since 1986"
+	# docx = nlp(raw_text)
+	# html = displacy.render(docx,style="ent")
+	# html = html.replace("\n\n","\n")
+	# result = HTML_WRAPPER.format(html)
+
+	return render_template('index.html')
+
+
+@app.route('/extract',methods=["GET","POST"])
+def extract():
+	if request.method == 'POST':
+		raw_text = request.form['rawtext']
+		docx = nlp(raw_text)
+		html = displacy.render(docx,style="ent")
+		html = html.replace("\n\n","\n")
+		result = HTML_WRAPPER.format(html)
+
+	return render_template('result.html',rawtext=raw_text,result=result)
+
+
+@app.route('/previewer')
+def previewer():
+	return render_template('previewer.html')
+
+@app.route('/preview',methods=["GET","POST"])
+def preview():
+	if request.method == 'POST':
+		newtext = request.form['newtext']
+		result = newtext
+
+	return render_template('preview.html',newtext=newtext,result=result)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
